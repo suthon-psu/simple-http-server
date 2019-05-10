af3b8700a4d0d8e5befb51f18fdf8e5ff7060359
Suthon <suthon@gmail.com>
2019-05-10 14:15:05 +0700

    initial

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 80
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, "0.0.0.0",
+ () => console.log(`Simple HTTP server listening on port ${port}!`))
